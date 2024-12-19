4a1bec02aae0e419dbcbdcf44ced1cd7d5ad1963
Vineet Bhasin <bhasinvineet49@gmail.com>
2024-12-19 09:45:00 +0530

    implementing cors

---
+++ notes-frontend/src/components/Footer.jsx
@@ -0,0 +1,15 @@
+const Footer = () => {
+  const footerStyle = {
+    color: "#adb5bd",
+    fontStyle: "italic",
+    fontSize: 16,
+  };
+  return (
+    <div style={footerStyle}>
+      <br />
+      <em>Note App - Vineet &copy;</em>
+    </div>
+  );
+};
+
+export default Footer;
