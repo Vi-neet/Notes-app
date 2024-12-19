ce5595ff617e1a0c35af9942af8135f9daaf26f2
Vineet Bhasin <bhasinvineet49@gmail.com>
2024-12-19 10:26:38 +0530

    minor syntax fix

--- notes-backend/index.js
+++ notes-backend/index.js
@@ -69,7 +69,7 @@ const unknownEndpoint = (req, res) => {
   res.status(404).send({ error: "unknown endpoint" });
 };
 app.use(unknownEndpoint);
-const PORT = process.eventNames.PORT || 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
